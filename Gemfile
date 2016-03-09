a337619723794946d01e9dfa61c3d7f05932c8ae
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2016-03-09 13:02:48 -0400

    Try logging sidekiq data

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '2.3.0'
+
+gem 'sidekiq', require: 'sidekiq/api'
+gem 'dogapi'
+gem 'clockwork'
---
+++ clock.rb
@@ -0,0 +1,28 @@
+require 'bundler/setup'
+Bundler.require
+
+module Clockwork
+  Dog = Dogapi::Client.new(ENV.fetch('DD_API_KEY'), nil, ENV.fetch('DD_HOST'))
+
+  handler do |job|
+    Dog.batch_metrics do
+      stats = Sidekiq::Stats.new
+
+      stats.queues.each do |queue, size|
+        Dog.emit_point("sidekiq.queues.#{queue}.size", size)
+      end
+
+      Dog.emit_points('sidekiq.failed', stats.failed)
+      Dog.emit_points('sidekiq.enqueued', stats.enqueued)
+      Dog.emit_points('sidekiq.processed', stats.processed)
+
+      Dog.emit_points('sidekiq.processes_size', stats.processes_size)
+      Dog.emit_points('sidekiq.scheduled_size', stats.scheduled_size)
+      Dog.emit_points('sidekiq.retry_size', stats.retry_size)
+      Dog.emit_points('sidekiq.dead_size', stats.dead_size)
+      Dog.emit_points('sidekiq.workers_size', stats.workers_size)
+    end
+  end
+
+  every(15.seconds, 'sidekiq')
+end
